6835baf96476ce386f04c1b492eafdbb2b834a85
Carlos Zalpa <nedhel.90@gmail.com>
2023-09-18 00:17:18 -0700

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+//This function is for open the modal menu when is a small device
 function openMenu(x) {
     x.classList.toggle("change");
     document.getElementById("menusmall").classList.toggle("show");
 }
 
+//When the page is loaded I add the events
 window.addEventListener("load", () => {
     document.getElementById("banner-img").style.transform = "scale(100%)";
     let currenY = 0;
+    //This events works when you use the scroll
     window.addEventListener("scroll", () => {
         let y = window.scrollY;
         if (y < currenY) {
